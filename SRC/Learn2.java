5f9e2490f1f52fd7a6bdd376e386a4fee287b178
firoz86 <sandsalt86@gmail.com>
2023-12-17 20:09:59 +0530

    First Github Committ

--- SRC/Learn2.java
+++ SRC/Learn2.java
@@ -3,5 +3,12 @@ package SRC;
 public class Learn2 {
     public static void main(String[] args) {
         System.out.println("Testing Session");
+
+        if(10<9)
+            System.out.println("Good");
+        else {
+            System.out.println("Not Good");
+        }
+
     }
 }
